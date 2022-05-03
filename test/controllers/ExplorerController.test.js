a8af2a924940ba97c3d11f201cbabcb8e3e5f614
David Patiño <david.andrespr5@gmail.com>
2022-05-03 08:02:56 -0500

    40. Agregando pruebas unitarias a ExplorerController

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -13,3 +13,21 @@ describe('Test para Class ExplorerController',()=>{
         expect(numExplorers).toBe(10)
     })
 })
+describe("Test ExplorerController - Metodo (getExplorersFizzbuzz)",()=>{
+    test('1. Numero divisible entre 3 y 5',()=>{
+        const fizznumber= ExplorerController.getExploerersFizzbuzz(15);
+        expect(fizznumber).toBe('FIZZBUZZ');
+    })
+    test('2. Numero divisible entre 3',()=>{
+        const fizznumber= ExplorerController.getExploerersFizzbuzz(3);
+        expect(fizznumber).toBe('FIZZ');
+    })
+    test('3. Numero divisible entre 5',()=>{
+        const fizznumber= ExplorerController.getExploerersFizzbuzz(5);
+        expect(fizznumber).toBe('BUZZ');
+    })
+    test('4. Numero no divisible entre 3 y 5',()=>{
+        const fizznumber= ExplorerController.getExploerersFizzbuzz(1);
+        expect(fizznumber).toBe(1);
+    });
+});
